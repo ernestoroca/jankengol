9b2dd5a4c8cf46f4133f96f845a39a1541860dc2
ernestoroca <ernesto.roca@gmail.com>
2020-03-14 17:45:00 -0400

    Update cliente.js

--- cliente.js
+++ cliente.js
@@ -1,7 +1,17 @@
 function iniciar(){
-  
+  firebase.auth().signInAnonymously();
 }
 
+var firebaseUID = "";
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    var isAnonymous = user.isAnonymous;
+    firebaseUID = user.uid;
+  } else {
+    firebase.auth().signInAnonymously();
+  }
+});
+
 function gestorOrientacion(){
-  alert(screen.orientation.angle);
+  alert(window.screen.orientation.angle);
 }
