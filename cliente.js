b1af77dbea957e9fbdbcd2ed801d208e7bb0f0d1
ernestoroca <ernesto.roca@gmail.com>
2020-03-14 18:32:24 -0400

    Update cliente.js

--- cliente.js
+++ cliente.js
@@ -1,15 +1,7 @@
 var firebaseUID = "";
 
 function iniciar(){
-  var firebaseConfig = {
-    apiKey: "AIzaSyAE0BFC11pL8LAsmzfKojGAkxiwt5-sbBo",
-    authDomain: "jankengol.firebaseapp.com",
-    databaseURL: "https://jankengol.firebaseio.com",
-    projectId: "jankengol",
-    storageBucket: "jankengol.appspot.com",
-    messagingSenderId: "225043875998",
-    appId: "1:225043875998:web:d204d20da25101fe5e538f"
-  };
+  
   firebase.initializeApp(firebaseConfig);
   //var database = firebase.database();
   firebase.auth().signInAnonymously();
