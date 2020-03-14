2c27c610683126e29c19538011cee80e36ff3f36
ernestoroca <ernesto.roca@gmail.com>
2020-03-14 18:05:24 -0400

    Update cliente.js

--- cliente.js
+++ cliente.js
@@ -1,27 +1,27 @@
+var firebaseUID = "";
+
 function iniciar(){
   var firebaseConfig = {
-        apiKey: "AIzaSyAE0BFC11pL8LAsmzfKojGAkxiwt5-sbBo",
-        authDomain: "jankengol.firebaseapp.com",
-        databaseURL: "https://jankengol.firebaseio.com",
-        projectId: "jankengol",
-        storageBucket: "jankengol.appspot.com",
-        messagingSenderId: "225043875998",
-        appId: "1:225043875998:web:d204d20da25101fe5e538f"
+    apiKey: "AIzaSyAE0BFC11pL8LAsmzfKojGAkxiwt5-sbBo",
+    authDomain: "jankengol.firebaseapp.com",
+    databaseURL: "https://jankengol.firebaseio.com",
+    projectId: "jankengol",
+    storageBucket: "jankengol.appspot.com",
+    messagingSenderId: "225043875998",
+    appId: "1:225043875998:web:d204d20da25101fe5e538f"
   };
   firebase.initializeApp(firebaseConfig);
   //var database = firebase.database();
   firebase.auth().signInAnonymously();
+  firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      var isAnonymous = user.isAnonymous;
+      firebaseUID = user.uid;
+    } else {
+      firebase.auth().signInAnonymously();
+    }
+  });
 }
-
-var firebaseUID = "";
-firebase.auth().onAuthStateChanged(function(user) {
-  if (user) {
-    var isAnonymous = user.isAnonymous;
-    firebaseUID = user.uid;
-  } else {
-    firebase.auth().signInAnonymously();
-  }
-});
 
 function gestorOrientacion(){
   alert(window.screen.orientation.angle);
