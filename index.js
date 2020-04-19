fdf840bea86e912d9da766404b4abcf03c37f8df
ernestoroca <ernesto.roca@gmail.com>
2020-04-19 16:45:25 -0400

    Update index.js

--- index.js
+++ index.js
@@ -139,3 +139,91 @@ exports.setPosicion = functions.https.onCall((param, context) => {
     return false;
   }); 
 });
+
+exports.setEquipo = functions.https.onCall((param, context) => {
+  const userId = context.auth.uid;
+  var database = admin.database();
+  var usuarioRef = database.ref('usuarios/' + userId);
+  return usuarioRef.once('value').then((snapshot) => {
+    var datos = snapshot.val();
+    function validar(){
+      function jugadorInvalido(codJugador){
+        //valida que sea mi jugador y no este en el equipo dos veces.
+        if (codJugador !== "0"){
+          if (datos.jugadores.indexOf(codJugador) >= 0){//es mi jugador
+            if (equipo.indexOf(codJugador) < 0){ //no esta en el equipo, todavia
+              equipo.push(codJugador);
+              return false;
+            }
+          }
+          return true;
+        }
+        return false;//es suplente
+      }
+      var equipo = [];
+
+      //arquero;
+      if (jugadorInvalido(param.arquero)){
+        return false;
+      }
+
+      //defensa
+      var lng = param.defensa.length;
+      var lngt = lng;
+      if (lng<2 || lng>4){
+        return false;
+      }
+      var i;
+      for(i=0;i<lng;i++){
+        if (jugadorInvalido(param.defensa[i])){
+          return false;
+        }
+      }
+
+      //medio
+      lng = param.medio.length;
+      lngt += lng;
+      if (lng<2 || lng>4){
+        return false;
+      }
+      for(i=0;i<lng;i++){
+        if (jugadorInvalido(param.medio[i])){
+          return false;
+        }
+      }
+
+      //ataque
+      lng = param.ataque.length;
+      lngt += lng;
+      if (lng<2 || lng>4){
+        return false;
+      }
+      if (lngt>10){
+        return false;
+      }
+      for(i=0;i<lng;i++){
+        if (jugadorInvalido(param.ataque[i])){
+          return false;
+        }
+      }
+      return true;
+    }
+    if (datos){
+      if (validar()){
+        usuarioRef.update({
+          arquero: param.arquero,
+          defensa: param.defensa.slice(),
+          medio: param.defensa.slice(),
+          ataque: param.ataque.slice(),
+        });
+        return true;
+      } else {
+        return false;
+      }
+    } else {
+      return false;
+    }
+  }).catch((error)=>{
+    return false;
+  });
+});
